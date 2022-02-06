ca3ab53ca2f79fdda08205a5c1ded1bbfbd3af14
davidmarinho <davidmarinho@outlook.pt>
2022-02-06 15:32:35 +0000

    Update test.py
    code fixed

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ print ("Hello World");
 n1 = float(input('Enter first number : '))
 n2 = float(input('Enter second number : '))
 
-sum = n1 + n2
-print('Sum =', sum)
+sum_nrs = n1 + n2
+print('Sum =', sum_nrs)
